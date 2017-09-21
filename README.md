8db6909788a2c314d66199669d1e0ebfa972e144
Stu Stirling <stu@boxshark.co.uk>
2017-09-21 19:39:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#Ribot Viewer
+
+This project is a chance for me to try out a lot of new things.
+
+- Android Studio 3.0
+- Room Persistence Library
+- ViewModel
+
+It will be developed using TDD with clean coding principles in mind. 
+Code will be separated between 3 modules; data, domain and app/presentation.
+
+
---
+++ app/src/androidTest/java/com/stustirling/ribotviewer/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.stustirling.ribotviewer
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.stustirling.ribotviewer", appContext.packageName)
+    }
+}
